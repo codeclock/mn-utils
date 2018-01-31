e7aede55ab051aeba7a97bc1508a6515e67a447c
codeclock <34115280+codeclock@users.noreply.github.com>
2018-01-31 11:36:33 +0700

    Update vps-setup.sh
    use absolute path everywhere, it seems older versions of sudo didn't change working directory properly
    check if initial stop of suppod was succesful and retry if not
    wait before restarting suppod to avoid error on backup already exists
    add some checks that the user actually updated the variables (later should check for all vars and maybe add some data sanity checking i.e correct private key length

--- vps-setup.sh
+++ vps-setup.sh
@@ -15,6 +15,11 @@ MNPRIKEY=YOUR_MASTERNODER_PRIVATE_KEY
 VPSIP=IP_OF_YOUR_VPS
 #don't edit anything beyond this point
 
+if [[ "$NEWUSER" = "OS_USER" ]] || [[ "$RPCUSER" = "USER_NAME_OF_YOUR_CHOICE" ]]\
+ || [[ "$RPCPW" = "PASSWORD_OF_YOUR_CHOICE" ]] ; then echo "Please UPDATE the variables, before running the script!"; exit 1; fi
+
+
+
 
 useradd -m $NEWUSER
 apt-get update -y
@@ -23,18 +28,26 @@ apt-get install sudo git python-virtualenv -y
 
 mv suppo* /home/$NEWUSER
 sudo -i -u $NEWUSER /home/$NEWUSER/suppod --daemon
-sudo -i -u $NEWUSER ./suppo-cli stop
 
-sudo -i -u $NEWUSER echo "rpcuser=$RPCUSER" > .suppocore/suppo.conf
-sudo -i -u $NEWUSER echo "rpcpassword=$RPCPW" >> .suppocore/suppo.conf
-sudo -i -u $NEWUSER echo "rpcallowip=127.0.0.1" >> .suppocore/suppo.conf
-sudo -i -u $NEWUSER echo "rpcport=7778" >> .suppocore/suppo.conf
-sudo -i -u $NEWUSER echo "daemon=1" >> .suppocore/suppo.conf
-sudo -i -u $NEWUSER echo "server=1" >> .suppocore/suppo.conf
-sudo -i -u $NEWUSER echo "maxconnections=64" >> .suppocore/suppo.conf
-sudo -i -u $NEWUSER echo "masternode=1" >> .suppocore/suppo.conf
-sudo -i -u $NEWUSER echo "masternodeprivkey=$MNPRIKEY" >> .suppocore/suppo.conf
-sudo -i -u $NEWUSER echo "externalip=$VPSIP" >> .suppocore/suppo.conf
+stop_suppod(){
+echo -e "Waiting for suppod to finish basic setup."
+
+sleep 30
+sudo -i -u $NEWUSER /home/$NEWUSER/suppo-cli stop &>/dev/null
+}
+stop_suppod
+while [ $? != 0 ]; do stop_suppod; done
+
+sudo -i -u $NEWUSER echo "rpcuser=$RPCUSER" > /home/$NEWUSER/.suppocore/suppo.conf
+sudo -i -u $NEWUSER echo "rpcpassword=$RPCPW" >> /home/$NEWUSER/.suppocore/suppo.conf
+sudo -i -u $NEWUSER echo "rpcallowip=127.0.0.1" >> /home/$NEWUSER/.suppocore/suppo.conf
+sudo -i -u $NEWUSER echo "rpcport=7778" >> /home/$NEWUSER/.suppocore/suppo.conf
+sudo -i -u $NEWUSER echo "daemon=1" >> /home/$NEWUSER/.suppocore/suppo.conf
+sudo -i -u $NEWUSER echo "server=1" >> /home/$NEWUSER/.suppocore/suppo.conf
+sudo -i -u $NEWUSER echo "maxconnections=64" >> /home/$NEWUSER/.suppocore/suppo.conf
+sudo -i -u $NEWUSER echo "masternode=1" >> /home/$NEWUSER/.suppocore/suppo.conf
+sudo -i -u $NEWUSER echo "masternodeprivkey=$MNPRIKEY" >> /home/$NEWUSER/.suppocore/suppo.conf
+sudo -i -u $NEWUSER echo "externalip=$VPSIP" >> /home/$NEWUSER/.suppocore/suppo.conf
 
 sudo -i -u $NEWUSER git clone https://github.com/codeclock/sentinel.git /home/$NEWUSER/.suppocore/sentinel
 
@@ -43,13 +56,14 @@ cd /home/$NEWUSER/.suppocore/sentinel
 sudo -u $NEWUSER virtualenv venv 
 sudo -u $NEWUSER venv/bin/pip install -r requirements.txt
 
-sleep 10
-sudo -i -u $NEWUSER ./suppod
+echo -e "Waiting for cleanup."
+sleep 60
+sudo -i -u $NEWUSER /home/$NEWUSER/suppod
 
 check_sync(){
 echo -e "Waiting for masternode synchronization. This can take some time."
 sleep 30
-sudo -i -u $NEWUSER ./suppo-cli mnsync status | grep "MASTERNODE_SYNC_FINISHED" &>/dev/null
+sudo -i -u $NEWUSER /home/$NEWUSER/suppo-cli mnsync status | grep "MASTERNODE_SYNC_FINISHED" &>/dev/null
 }
 check_sync
 while [ $? != 0 ]; do check_sync; done
